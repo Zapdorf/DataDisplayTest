076e1287d37b54ce240516bcf18f646894a49eb0
Andrew Vandercar <112919868+Zapdorf@users.noreply.github.com>
2024-08-07 22:10:57 -0500

    Initial commit

---
+++ src/main/java/AndrewVandercar/DataDisplayTest/DataDisplayTestApplication.java
@@ -0,0 +1,13 @@
+package AndrewVandercar.DataDisplayTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataDisplayTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataDisplayTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/AndrewVandercar/DataDisplayTest/DataDisplayTestApplicationTests.java
@@ -0,0 +1,13 @@
+package AndrewVandercar.DataDisplayTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataDisplayTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
